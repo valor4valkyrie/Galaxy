356cb138905fba56ad90a6af4369a1a16b6088ef
Chris Pemberton <c.george.pemberton@gmail.com>
2021-09-08 00:54:35 -0500

    Refactored Numerals to have 'id's

--- src/main/java/com/example/galaxy/services/Numerals.java
+++ src/main/java/com/example/galaxy/services/Numerals.java
@@ -2,13 +2,19 @@ package com.example.galaxy.services;
 
 public enum Numerals {
 
-    I( 1), V( 5), X( 10), L( 50), C( 100),
-    D( 500), M( 1000);
+    I( "I",1), V("V", 5), X("X", 10), L("L", 50), C("C", 100),
+    D("D", 500), M("M", 1000);
 
     private final int value;
+    private final String id;
 
-    Numerals(int value) {
+    Numerals(String id, int value) {
+        this.id = id;
         this.value = value;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public int getValue() {
